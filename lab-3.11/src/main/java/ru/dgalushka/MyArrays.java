fcd1aedd264373d4a270d5bc3998651ab4467692
Dzeksid99 <50305214+Dzeksid99@users.noreply.github.com>
2024-12-16 14:17:43 +0300

    Add files via upload

---
+++ lab-3.11/src/main/java/ru/dgalushka/MyArrays.java
@@ -0,0 +1,230 @@
+package ru.dgalushka;
+
+import java.util.Comparator;
+
+public class MyArrays {
+
+    public static final double EPS = 1e-9;
+
+    // Поиск по всему массиву.
+    // 1.
+    public static int binarySearch(byte[] a, byte key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // Поиск по массиву на полуинтервале [fromIndex, toIndex).
+    // 2.
+    public static int binarySearch(byte[] a, int fromIndex, int toIndex, byte key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1; // Середина массива.
+            byte midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1; // Ключ в правой половине.
+            } else if (midVal > key) {
+                high = mid - 1; // Ключ в левой половине.
+            } else {
+                return mid; // Ключ найден.
+            }
+        }
+        return -(low + 1);  // Ключ не найден.
+    }
+
+    // 3.
+    public static int binarySearch(char[] a, char key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 4.
+    public static int binarySearch(char[] a, int fromIndex, int toIndex, char key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            char midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    // 5.
+    public static int binarySearch(double[] a, double key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 6.
+    public static int binarySearch(double[] a, int fromIndex, int toIndex, double key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            double midVal = a[mid];
+
+            int cmp = compare(midVal, key);
+
+            if (cmp < 0) {
+                low = mid + 1;
+            } else if (cmp > 0) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    // 7.
+    public static int binarySearch(float[] a, float key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 8.
+    public static int binarySearch(float[] a, int fromIndex, int toIndex, float key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            float midVal = a[mid];
+
+            int cmp = compare(midVal, key);
+
+            if (cmp < 0) {
+                low = mid + 1;
+            } else if (cmp > 0) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    // 9.
+    public static int binarySearch(int[] a, int key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 10.
+    public static int binarySearch(int[] a, int fromIndex, int toIndex, int key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    // 11.
+    public static int binarySearch(long[] a, long key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 12.
+    public static int binarySearch(long[] a, int fromIndex, int toIndex, long key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            long midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    // 13.
+    public static int binarySearch(short[] a, short key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 14.
+    public static int binarySearch(short[] a, int fromIndex, int toIndex, short key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            short midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    public static <T> int binarySearch(T[] a, T key, Comparator<? super T> c) {
+        return binarySearch(a, 0, a.length, key, c);
+    }
+
+    public static <T> int binarySearch(T[] a, int fromIndex, int toIndex, T key, Comparator<? super T> c) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            T midVal = a[mid];
+
+            int cmp = c.compare(midVal, key);
+
+            if (cmp < 0) {
+                low = mid + 1;
+            } else if (cmp > 0) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    private static int compare(double a, double b) {
+        if (Math.abs(a - b) < EPS) {
+            return 0;
+        } else {
+            return a - b > 0 ? 1 : -1;
+        }
+    }
+
+    private static int compare(float a, float b) {
+        if (Math.abs(a - b) < EPS) {
+            return 0;
+        } else {
+            return a - b > 0 ? 1 : -1;
+        }
+    }
+}
---
+++ lab-3.11/src/test/java/ru/dgalushka/MyArraysTest.java
@@ -0,0 +1,348 @@
+package ru.dgalushka;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MyArraysTest {
+
+    public static final int NUM_TESTS = 100_000; // Количество тестов на каждый тип.
+    public static final int MAX_ARRAY_SIZE = 100; // Максимальный размер массива на каждую итерацию теста.
+
+    public Random random = new Random();
+
+    @Test
+    public void testByteBinarySearchRandomArrays() {
+        for (int i = 0; i < NUM_TESTS; i++) {
+            random = new Random();
+
+            byte[] array = new byte[random.nextInt(MAX_ARRAY_SIZE)];
+            int len = array.length;
+
+            for (int j = 0; j < len; j++) {
+                array[j] = (byte) random.nextInt(256); // Генерация случайного байта.
+            }
+
+            // Предварительная сортировка массива.
+            Arrays.sort(array);
+
+            // Берем ключ для поиска либо из массива, либо случайный.
+            byte key = random.nextBoolean() && len > 0
+                    ? array[random.nextInt(len)]
+                    : (byte) random.nextInt(256);
+
+            // Проверка всего массива.
+            assertEquals(
+                    Arrays.binarySearch(array, key),
+                    MyArrays.binarySearch(array, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key);
+
+            int fromIndex = len > 1
+                    ? random.nextInt(len / 2)
+                    : 0;
+            int toIndex = len > 1
+                    ? fromIndex + random.nextInt(len / 2)
+                    : len == 1 ? 1 : 0;
+
+            // Проверка для полуинтервала массива.
+            assertEquals(
+                    Arrays.binarySearch(array, fromIndex, toIndex, key),
+                    MyArrays.binarySearch(array, fromIndex, toIndex, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key +
+                            ", fromIndex: " + fromIndex +
+                            ", toIndex: " + toIndex);
+        }
+    }
+
+    @Test
+    public void testCharBinarySearchRandomArrays() {
+        for (int i = 0; i < NUM_TESTS; i++) {
+            random = new Random();
+
+            char[] array = new char[random.nextInt(MAX_ARRAY_SIZE)];
+            int len = array.length;
+
+            for (int j = 0; j < len; j++) {
+                array[j] = (char) random.nextInt(65536);
+            }
+
+            Arrays.sort(array);
+
+            char key = random.nextBoolean() && len > 0
+                    ? array[random.nextInt(len)]
+                    : (char) random.nextInt(65536);
+
+            assertEquals(
+                    Arrays.binarySearch(array, key),
+                    MyArrays.binarySearch(array, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key);
+
+            int fromIndex = len > 1
+                    ? random.nextInt(len / 2)
+                    : 0;
+            int toIndex = len > 1
+                    ? fromIndex + random.nextInt(len / 2)
+                    : len == 1 ? 1 : 0;
+
+            assertEquals(
+                    Arrays.binarySearch(array, fromIndex, toIndex, key),
+                    MyArrays.binarySearch(array, fromIndex, toIndex, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key +
+                            ", fromIndex: " + fromIndex +
+                            ", toIndex: " + toIndex);
+        }
+    }
+
+    @Test
+    public void testDoubleBinarySearchRandomArrays() {
+        for (int i = 0; i < NUM_TESTS; i++) {
+            random = new Random();
+
+            double[] array = new double[random.nextInt(MAX_ARRAY_SIZE)];
+            int len = array.length;
+
+            for (int j = 0; j < len; j++) {
+                array[j] = random.nextDouble();
+            }
+
+            Arrays.sort(array);
+
+            double key = random.nextBoolean() && len > 0
+                    ? array[random.nextInt(len)]
+                    : random.nextDouble();
+
+            assertEquals(
+                    Arrays.binarySearch(array, key),
+                    MyArrays.binarySearch(array, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key);
+
+            int fromIndex = len > 1
+                    ? random.nextInt(len / 2)
+                    : 0;
+            int toIndex = len > 1
+                    ? fromIndex + random.nextInt(len / 2)
+                    : len == 1 ? 1 : 0;
+
+            assertEquals(
+                    Arrays.binarySearch(array, fromIndex, toIndex, key),
+                    MyArrays.binarySearch(array, fromIndex, toIndex, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key +
+                            ", fromIndex: " + fromIndex +
+                            ", toIndex: " + toIndex);
+        }
+    }
+
+    @Test
+    public void testFloatBinarySearchRandomArrays() {
+        for (int i = 0; i < NUM_TESTS; i++) {
+            random = new Random();
+
+            float[] array = new float[random.nextInt(MAX_ARRAY_SIZE)];
+            int len = array.length;
+
+            for (int j = 0; j < len; j++) {
+                array[j] = random.nextFloat();
+            }
+
+            Arrays.sort(array);
+
+            float key = random.nextBoolean() && len > 0
+                    ? array[random.nextInt(len)]
+                    : random.nextFloat();
+
+            assertEquals(
+                    Arrays.binarySearch(array, key),
+                    MyArrays.binarySearch(array, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key);
+
+            int fromIndex = len > 1
+                    ? random.nextInt(len / 2)
+                    : 0;
+            int toIndex = len > 1
+                    ? fromIndex + random.nextInt(len / 2)
+                    : len == 1 ? 1 : 0;
+
+            assertEquals(
+                    Arrays.binarySearch(array, fromIndex, toIndex, key),
+                    MyArrays.binarySearch(array, fromIndex, toIndex, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key +
+                            ", fromIndex: " + fromIndex +
+                            ", toIndex: " + toIndex);
+        }
+    }
+
+    @Test
+    public void testIntBinarySearchRandomArrays() {
+        for (int i = 0; i < NUM_TESTS; i++) {
+            random = new Random();
+
+            int[] array = new int[random.nextInt(MAX_ARRAY_SIZE)];
+            int len = array.length;
+
+            for (int j = 0; j < len; j++) {
+                array[j] = random.nextInt();
+            }
+
+            Arrays.sort(array);
+
+            int key = random.nextBoolean() && len > 0
+                    ? array[random.nextInt(len)]
+                    : random.nextInt();
+
+            assertEquals(
+                    Arrays.binarySearch(array, key),
+                    MyArrays.binarySearch(array, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key);
+
+            int fromIndex = len > 1
+                    ? random.nextInt(len / 2)
+                    : 0;
+            int toIndex = len > 1
+                    ? fromIndex + random.nextInt(len / 2)
+                    : len == 1 ? 1 : 0;
+
+            assertEquals(
+                    Arrays.binarySearch(array, fromIndex, toIndex, key),
+                    MyArrays.binarySearch(array, fromIndex, toIndex, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key +
+                            ", fromIndex: " + fromIndex +
+                            ", toIndex: " + toIndex);
+        }
+    }
+
+    @Test
+    public void testLongBinarySearchRandomArrays() {
+        for (int i = 0; i < NUM_TESTS; i++) {
+            random = new Random();
+
+            long[] array = new long[random.nextInt(MAX_ARRAY_SIZE)];
+            int len = array.length;
+
+            for (int j = 0; j < len; j++) {
+                array[j] = random.nextLong();
+            }
+
+            Arrays.sort(array);
+
+            long key = random.nextBoolean() && len > 0
+                    ? array[random.nextInt(len)]
+                    : random.nextLong();
+
+            assertEquals(
+                    Arrays.binarySearch(array, key),
+                    MyArrays.binarySearch(array, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key);
+
+            int fromIndex = len > 1
+                    ? random.nextInt(len / 2)
+                    : 0;
+            int toIndex = len > 1
+                    ? fromIndex + random.nextInt(len / 2)
+                    : len == 1 ? 1 : 0;
+
+            assertEquals(
+                    Arrays.binarySearch(array, fromIndex, toIndex, key),
+                    MyArrays.binarySearch(array, fromIndex, toIndex, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key +
+                            ", fromIndex: " + fromIndex +
+                            ", toIndex: " + toIndex);
+        }
+    }
+
+    @Test
+    public void testShortBinarySearchRandomArrays() {
+        for (int i = 0; i < NUM_TESTS; i++) {
+            random = new Random();
+
+            short[] array = new short[random.nextInt(MAX_ARRAY_SIZE)];
+            int len = array.length;
+
+            for (int j = 0; j < len; j++) {
+                array[j] = (short) random.nextInt(65536);
+            }
+
+            Arrays.sort(array);
+
+            short key = random.nextBoolean() && len > 0
+                    ? array[random.nextInt(len)]
+                    : (short) random.nextInt(65536);
+
+            assertEquals(
+                    Arrays.binarySearch(array, key),
+                    MyArrays.binarySearch(array, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key);
+
+            int fromIndex = len > 1
+                    ? random.nextInt(len / 2)
+                    : 0;
+            int toIndex = len > 1
+                    ? fromIndex + random.nextInt(len / 2)
+                    : len == 1 ? 1 : 0;
+
+            assertEquals(
+                    Arrays.binarySearch(array, fromIndex, toIndex, key),
+                    MyArrays.binarySearch(array, fromIndex, toIndex, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key +
+                            ", fromIndex: " + fromIndex +
+                            ", toIndex: " + toIndex);
+        }
+    }
+
+    @Test
+    public void testGenericBinarySearchRandomArrays() {
+        for (int i = 0; i < NUM_TESTS; i++) {
+            random = new Random();
+
+            Point[] array = new Point[random.nextInt(MAX_ARRAY_SIZE)];
+            int len = array.length;
+
+            for (int j = 0; j < len; j++) {
+                array[j] = Point.getRandom(random);
+            }
+
+            Arrays.sort(array, Point.comparator);
+
+            Point key = random.nextBoolean() && len > 0
+                    ? array[random.nextInt(len)]
+                    : Point.getRandom(random);
+
+            assertEquals(
+                    Arrays.binarySearch(array, key, Point.comparator),
+                    MyArrays.binarySearch(array, key, Point.comparator),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key);
+
+            int fromIndex = len > 1
+                    ? random.nextInt(len / 2)
+                    : 0;
+            int toIndex = len > 1
+                    ? fromIndex + random.nextInt(len / 2)
+                    : len == 1 ? 1 : 0;
+
+            assertEquals(
+                    Arrays.binarySearch(array, fromIndex, toIndex, key, Point.comparator),
+                    MyArrays.binarySearch(array, fromIndex, toIndex, key, Point.comparator),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key +
+                            ", fromIndex: " + fromIndex +
+                            ", toIndex: " + toIndex);
+        }
+    }
+}
